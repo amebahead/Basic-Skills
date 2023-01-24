3d226994ade860a3c9cd9dd0ba008b23f263ddfe
MacDole <>
2023-01-24 16:23:58 +0900

    feat: test another example

--- DataStructure/MST-Kruskal.playground/Contents.swift
+++ DataStructure/MST-Kruskal.playground/Contents.swift
@@ -6,18 +6,44 @@
 
 import Foundation
 
-let vertices = ["A", "B", "C", "D"]
+/*
+ let vertices = ["A", "B", "C", "D"]
+ let edges = [
+ (5,  "A", "B"),
+ (1,  "A", "C"),
+ (10, "A", "D"),
+ (5,  "B", "A"),
+ (3,  "B", "D"),
+ (1,  "C", "A"),
+ (8,  "C", "D"),
+ (10, "D", "A"),
+ (3,  "D", "B"),
+ (8,  "D", "C"),
+ ]
+ */
+
+let vertices = ["A", "B", "C", "D", "E", "F", "G", "H"]
 let edges = [
-  (5,  "A", "B"),
-  (1,  "A", "C"),
-  (10, "A", "D"),
-  (5,  "B", "A"),
-  (3,  "B", "D"),
-  (1,  "C", "A"),
-  (8,  "C", "D"),
-  (10, "D", "A"),
-  (3,  "D", "B"),
-  (8,  "D", "C"),
+  (6, "A", "B"),
+  (5, "A", "C"),
+  (14, "A", "E"),
+  (10, "A", "F"),
+  (6, "B", "A"),
+  (4, "B", "C"),
+  (4, "C", "B"),
+  (5, "C", "A"),
+  (2, "C", "G"),
+  (9, "C", "D"),
+  (9, "D", "C"),
+  (14, "E", "A"),
+  (3, "E", "F"),
+  (3, "F", "E"),
+  (10, "F", "A"),
+  (8, "F", "G"),
+  (2, "G", "C"),
+  (8, "G", "F"),
+  (15, "G", "H"),
+  (15, "H", "G"),
 ]
 
 typealias edge = (Int, String, String)
@@ -42,20 +68,20 @@ func kruskal(vertices: [String], edges: [edge]) -> [edge] {
   }
 
   /*
-   func union(_ nodeV: String, _ nodeU: String) {
-   let rankV = find(nodeV)
-   let rankU = find(nodeU)
+  func union(_ nodeV: String, _ nodeU: String) {
+    let rankV = find(nodeV)
+    let rankU = find(nodeU)
 
-   if rankV > rankU {
-   parent[rankU] = rankV
-   } else {
-   parent[rankV] = nodeU
-   if rankV == rankU {
-   rank[nodeU]! += 1
-   }
-   }
-   }
-   */
+    if rankV > rankU {
+      parent[rankU] = rankV
+    } else {
+      parent[rankV] = nodeU
+      if rankV == rankU {
+        rank[nodeU]! += 1
+      }
+    }
+  }
+  */
 
   func union_renewal(_ nodeV: String, _ nodeU: String) {
     let rootV = find(nodeV)
